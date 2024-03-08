359cc85965c8dcb1bbfca3e13aa1665e1b3e8f5a
Kaspars Bleidelis <k.bleidelis@gmail.com>
2024-03-08 16:15:05 +0000

    matrix multiplication in numpy

--- MatrixMultiplication.py
+++ MatrixMultiplication.py
@@ -1,6 +1,10 @@
-print("hello mom")
-def sum(x, y):
-    return x+y
-print(sum(3,4))
+import numpy as np
 
-print("upper".upper() + "lower".lower())
+A_mat = np.array([[10, 20, 30], [100, 200, 300], [1, 2, 3]])
+B_mat = np.array([[1, 5],[10, 20], [100, 200]])
+
+print("AxB:\n")
+print((A_mat @ B_mat)) # 3x3 @ 3x2 ====> OK
+
+print("\nBxA:\n")
+print(B_mat @ A_mat) #3x2 @ 3x3 ====> Not OK (The order of matrices in multiplication matters.)
